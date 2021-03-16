0722ea2df282df0ee782b24a58aaa3332424d789
Naka0405 <naka04054840@gmail.com>
2021-03-16 12:15:35 +0200

    configure mapper

--- Banks/Banks.BusinessLogic/AutoMapper/MapperProfile.cs
+++ Banks/Banks.BusinessLogic/AutoMapper/MapperProfile.cs
@@ -1,15 +1,17 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Banks.Entities;
+using Banks.ViewModels.Models;
 
 namespace Banks.API.AutoMapper
 {
     public class MapperProfile:Profile 
     {
         public MapperProfile()
-        {  
+        {
+            CreateMap<Account, AccountVM>()
+                .ForMember(x => x.ClientFullName, opt => opt
+                  .MapFrom(src => src.Client.FirstName + " " + src.Client.LastName))
+                .ReverseMap();                
         }
     }
 }
---
+++ Banks/Banks.ViewModels/Models/AccountVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Banks.ViewModels.Models
+{
+    public class AccountVM:BaseViewModel
+    {
+        public int BankId { get; set; }
+        public string ClientFullName { get; set; }
+        public string Code { get; set; }
+        public string Account { get; set; }
+    }
+}
